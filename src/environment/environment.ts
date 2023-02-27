d02194a4edfc2793e2b8ca65862d3ea2519ae77d
Burhan Shaheen <hafiz.burhan834@gmail.com>
2023-02-27 12:02:29 +0500

    added production environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,1 +1,6 @@
 export const STORE_BASE_URL = 'https://fakestoreapi.com';
+
+export const environment ={
+    production:false,
+    BASE_URL: 'https://burhanrepos.github.io/ecommerce_web_store_with_angular_nodejs_stripe/'
+}
